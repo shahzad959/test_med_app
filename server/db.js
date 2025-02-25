d0f046c2fa4477e31b08f73cf306172477ae0b24
shahzad959 <shahzad_chauhan@hotmail.com>
2025-02-25 16:59:55 -0500

    Initial commit

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:9KgKV4VwGCcuHrLtQb2j3M03@172.21.13.172:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
