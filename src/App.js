2cdbb0bbc937c04168b7a18757f8b7032a8ce4be
shahzad959 <shahzad_chauhan@hotmail.com>
2025-02-26 14:58:50 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Importing necessary components from react-router-dom
+import Navbar from './Components/Navbar/Navbar'; // Import the Navbar component
+import Landing_Page from './Components/Landing_Page/Landing_Page'; // Import the Landing_Page component
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Landing_Page />} /> {/* Route for the Landing_Page */}
+          <Route path="/services" element={<h1>Services Page</h1>} /> {/* Example route for the services page */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
